481cb659388264d69748b187ac273ed8b8bdc1f7
Your Name <you@example.com>
2026-02-07 15:08:25 +0300

    feat: add layoyt for APP

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -1,12 +1,15 @@
 import {Header} from '@/common/components';
 import {Routing} from '@/common/routing';
+import s from './App.module.css'
 
 function App() {
 
     return (
         <>
             <Header/>
-            <Routing/>
+            <main className={s.layout}>
+                <Routing />
+            </main>
         </>
     )
 }
