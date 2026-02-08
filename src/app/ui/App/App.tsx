dd3d139a8490993b5a1ea4c98848f13f213b9353
Your Name <you@example.com>
2026-02-08 22:51:03 +0300

    chore: log playlists query result

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -1,17 +1,13 @@
 import {Header} from '@/common/components';
 import {Routing} from '@/common/routing';
-import {useFetchPlaylistsQuery} from '@/features/playlists/api/playlistsApi';
 import s from './App.module.css'
 
 function App() {
-    const { data, error, isLoading } = useFetchPlaylistsQuery({})
-    console.log({ data, error, isLoading })
     return (
         <>
             <Header/>
             <main className={s.layout}>
-                <Routing />
-
+                <Routing/>
             </main>
         </>
     )
--- src/features/playlists/ui/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage.tsx
@@ -1,11 +1,18 @@
 import {useFetchPlaylistsQuery} from '@/features/playlists/api/playlistsApi';
 
 export const PlaylistsPage = () => {
-    const res = useFetchPlaylistsQuery({})
-    console.log(res)
+    const {data} = useFetchPlaylistsQuery({})
+    console.log(data)
     return (
         <div>
             <h1>Playlists page</h1>
+            <p>
+                {data?.data.map((playlist) => {
+                    return <div key={playlist.id}>
+                        {playlist.attributes.title}
+                    </div>
+                })}
+            </p>
         </div>
     )
 }
