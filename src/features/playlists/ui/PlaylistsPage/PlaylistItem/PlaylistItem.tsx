62d538163c377a239224abbceb8bfc0ed9abf223
semenkr <samkrekotyn@gmail.com>
2026-02-14 16:46:08 +0300

    add upload file hook

--- src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx
@@ -1,6 +1,8 @@
+import {useUploadPlaylistCoverMutation} from '@/features/playlists/api/playlistsApi';
 import type {PlaylistData, UpdatePlaylistMutationArgs} from '@/features/playlists/api/playlistsApi.types'
 import {EditPlaylistForm} from '@/features/playlists/ui/PlaylistsPage/PlaylistItem/EditPlaylistForm/EditPlaylistForm';
 import defaultCover from '@/assets/images/default-playlist-cover.png'
+import type {ChangeEvent} from 'react';
 import s from './PlaylistItem.module.css'
 
 type Props = {
@@ -25,9 +27,32 @@ export const PlaylistItem = ({
     const src = originalCover ? originalCover?.url : defaultCover
     const description = playlist.attributes.description
 
+    const [uploadCover] = useUploadPlaylistCoverMutation()
+
+    const uploadCoverHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const maxSize = 1024 * 1024 // 1 MB
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
+
+        const file = event.target.files?.length && event.target.files[0]
+        if (!file) return
+
+        if (!allowedTypes.includes(file.type)) {
+            alert('Only JPEG, PNG or GIF images are allowed')
+            return
+        }
+
+        if (file.size > maxSize) {
+            alert(`The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`)
+            return
+        }
+
+        uploadCover({ playlistId: playlist.id, file })
+    }
+
     return (
         <article className={s.card}>
             <img src={src} alt={'cover'} className={s.cover}/>
+            <input type="file" accept="image/jpeg,image/png,image/gif" onChange={uploadCoverHandler} />
             <div className={s.cardBody}>
                 {isEditing ? (
                     <EditPlaylistForm
