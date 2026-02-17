71a226a876dfd6564d57f620de13ae259aed31b5
Your Name <you@example.com>
2026-02-17 14:49:05 +0300

    add paginations

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,3 +1,4 @@
+import {Pagination} from '@/common/components/Pagination/Pagination';
 import {useDebounceValue} from '@/common/hooks';
 import {useFetchPlaylistsQuery} from '@/features/playlists/api/playlistsApi'
 import {useState} from 'react'
@@ -8,8 +9,13 @@ import s from './PlaylistsPage.module.css'
 
 export const PlaylistsPage = () => {
     const [search, setSearch] = useState('')
+    const [currentPage, setCurrentPage] = useState(1)
     const debounceSearch = useDebounceValue(search)
-    const { data, error, isLoading } = useFetchPlaylistsQuery({ search: debounceSearch })
+    const { data, error, isLoading } = useFetchPlaylistsQuery({
+        search: debounceSearch,
+        pageNumber: currentPage,
+        pageSize: 5,
+    })
 
     const totalCount = data?.meta?.totalCount ?? 0
     const shownCount = data?.data.length ?? 0
@@ -52,6 +58,11 @@ export const PlaylistsPage = () => {
                     />
                 ))}
             </div>
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesCount={data?.meta.pagesCount || 1}
+            />
         </section>
     )
 }
