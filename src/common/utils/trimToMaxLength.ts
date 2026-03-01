902224e1895e201e8f93256bb8c67f5399940b89
Your Name <you@example.com>
2026-03-01 22:11:45 +0300

    pretire

--- src/common/utils/trimToMaxLength.ts
+++ src/common/utils/trimToMaxLength.ts
@@ -1,3 +1,7 @@
 export function trimToMaxLength(str: string, maxLength = 100): string {
-  return str.length > maxLength ? str.slice(0, maxLength - 3) + '...' : str
+  // ✂️ Если длина строки превышает максимальную —
+  // обрезаем её так, чтобы вместе с "..." итоговая длина не превышала maxLength
+  return str.length > maxLength
+      ? str.slice(0, maxLength - 3) + '...'
+      : str // ✅ Если строка короче лимита — возвращаем без изменений
 }
