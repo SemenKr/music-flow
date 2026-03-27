8733a17a58352dd0d123853454ff9b467dfe42cd
semenkr <samkrekotyn@gmail.com>
2026-03-27 18:25:49 +0300

    Reduce redundant auth requests on public pages

--- src/app/ui/MainPage/MainPage.tsx
+++ src/app/ui/MainPage/MainPage.tsx
@@ -1,12 +1,8 @@
-import { useGetMeQuery } from '@/features/auth/api/authApi'
-
 export const MainPage = () => {
-  const { data } = useGetMeQuery(undefined)
-
   return (
     <div>
       <h1>Main page</h1>
-      <div>login: {data?.login} </div>
+      <div>Discover playlists and tracks.</div>
     </div>
   )
 }
